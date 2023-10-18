68de89b89b1adf35851b14d5af7fd99359a97b58
upsure <ganeshkulkarni612@gmail.com>
2023-10-18 23:24:22 +0530

    namecommit

--- src/main/java/com/example/app/proxy/emigrantproxy.java
+++ src/main/java/com/example/app/proxy/emigrantproxy.java
@@ -3,7 +3,7 @@ package com.example.app.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="DEMO-SERVICE")
+@FeignClient(name="demo-service")
 public interface emigrantproxy {
 	
 	@GetMapping("/policy/checkapi")
